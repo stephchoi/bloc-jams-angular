31bc70adcdcb392dd42a60b12cf4841b3d84f602
Stephany Choi <s.choi417@gmail.com>
2017-07-22 10:22:15 -0400

    Add dynamic song info

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,12 +15,12 @@
        .state('album', {
          url: '/album',
          controller: 'AlbumCtrl as album',
-         templateUrl: 'templates/album.html'
+         templateUrl: '/templates/album.html'
        })
        .state('collection', {
          url: '/collection',
          controller: 'CollectionCtrl as collection',
-         templateUrl: 'templates/collection.html'
+         templateUrl: '/templates/collection.html'
        });
    }
 
