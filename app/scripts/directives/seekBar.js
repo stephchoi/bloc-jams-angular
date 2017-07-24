304381ceac611aa6efdd2f388ab0d7fbd0b91de7
Stephany Choi <s.choi417@gmail.com>
2017-07-24 13:40:04 -0400

    Add fill bar to seekbar directive

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -3,7 +3,23 @@
     return {
       templateUrl: '/template/directives/seek_bar.html',
       replace: true,
-      restrict: 'E'
+      restrict: 'E',
+      scope: { },
+      link: function(scope, element, attributes) {
+          scope.value = 0;
+          scope.max = 100;
+
+          var percentString = function() {
+            var value = scope.value;
+            var max = scope.max;
+            var percent = value / max * 100;
+            return percent + "%";
+          };
+
+          scope.fillStyle = function() {
+            return {width: percentString()};
+          };
+      }
     };
   }
 
