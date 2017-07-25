7e9847f42f0b8d72ece2a07b349eeb0cdfa2915d
Stephany Choi <s.choi417@gmail.com>
2017-07-25 15:21:17 -0400

    Debug collection

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -41,13 +41,14 @@
           };
 
           scope.trackThumb = function() {
-            $document.bind('mousemove.thumb', function(event){
+            $document.bind('mousemove.thumb', function(event) {
               var percent = calculatePercent(seekBar, event);
-              scope.$apply(function(){
+              scope.$apply(function() {
                 scope.value = percent * scope.max;
               });
             });
-            $document.bind('mouseup.thumb', function(){
+
+            $document.bind('mouseup.thumb', function() {
               $document.unbind('mousemove.thumb');
               $document.unbind('mouseup.thumb');
             });
