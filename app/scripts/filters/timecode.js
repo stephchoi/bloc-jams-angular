45559300c359a718211b28d09934c33977bae368
Stephany Choi <s.choi417@gmail.com>
2017-07-27 12:02:28 -0400

    Seconds to Minute conversion added

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,6 +1,20 @@
 (function() {
   function timecode() {
     return function(seconds) {
+      var seconds = Number.parseFloat(seconds);
+      var wholeSeconds = Math.floor(seconds);
+      var minutes = Math.floor(wholeSeconds / 60);
+      var remainingSeconds = wholeSeconds % 60;
+
+      var output = minutes + ':';
+
+    /*Adds placeholder 0 for when seconds are single digit #s*/
+      if(remainingSeconds < 10) {
+        output += '0'
+      }
+
+      output += remainingSeconds;
+
       return output;
     };
   }
